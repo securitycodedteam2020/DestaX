2564f9618f6611eea67d67c5d75f0ceed1d10866
securitycodedteam2020 <81040300+securitycodedteam2020@users.noreply.github.com>
2021-03-30 10:16:12 +0700

    Add files via upload

---
+++ DestaX.py
@@ -0,0 +1,33 @@
+import os
+import sys
+os.system('clear')
+os.system('figlet DestaX')
+author = "Desta Arya Nugraha"
+youtube = "Desta Tech"
+Instagram = "@desta.an"
+Team = "Security Coded Team"
+print("-----------------------------------------")
+print("Author " + ": " + author)
+print("YouTube " + ": " + youtube)
+print("Instagram " + ": " + Instagram)
+print("-----------------------------------------")
+nama = raw_input("masukkan nama : ")
+print("halo " + nama + "!" + "kita mulai ya gamenya!")
+hobi1 = "Hacking"
+hobi2 = "Penetration Testing"
+hobi3 = "Programming"
+faq1 = "Hacker Yang Handal"
+faq2 = "Pentester Yang Handal"
+faq3 = "Programmer Yang Handal"
+print("[?]Pilih Hobi Kamu")
+print("1. " + hobi1)
+print("2. " + hobi2)
+print("3. " + hobi3)
+pilih = raw_input("[?]Nomer: ")
+if pilih == "1":
+        print("wah keren ya " + nama + "!" + "hobi kamu " + hobi1 + "!" + "semoga kamu menjadi " + faq1)
+if pilih == "2":                                                                                                                                                                
+        print("wah keren ya " + nama + "!" + "hobi kamu " + hobi2 + "!" + "semoga kamu menjadi " + faq2)                                                                
+if pilih == "3":
+        print("wah keren ya " + nama + "!" + "hobi kamu " + hobi3 + "!" + "semoga kamu menjadi " + faq3)
+
